3418d73cf5b4d59479bf3073d70de03cce22fd39
febin123 <febsep96@gmail.com>
2023-12-23 16:08:13 +0530

    installed dependency(React.js & Tailwind css)

---
+++ tradezero/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+  return(
+    <>
+    <h1 className="text-3 font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+export default App
