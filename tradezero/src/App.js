42822412ea9a4fda4816579d078c37b33d42a022
febin123 <febsep96@gmail.com>
2023-12-26 15:49:45 +0530

    added router provider

--- tradezero/src/App.js
+++ tradezero/src/App.js
@@ -2,22 +2,40 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Software from "./components/Software";
-import Awards from "./components/Awards";
-import Account from "./components/Account";
-import Choose from "./components/Choose";
-import Footer from "./components/Footer";
+// import Awards from "./components/Awards";
+// import Account from "./components/Account";
+// import Choose from "./components/Choose";
+// import Footer from "./components/Footer";
+import {Outlet, createBrowserRouter} from "react-router-dom"
 
-function App(){
+const AppLayout=()=>{
   return(
-    <>
+    <div>
     <Navbar/>
-    <Hero/>
-    <Software/>
-    <Awards/>
+    <Outlet/>
+    {/* <Awards/>
     <Account/>
     <Choose/>
-    <Footer/>
-    </>
+    <Footer/> */}
+    </div>
   )
 }
-export default App
+export const appRouter=createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+    children:[
+        {
+            path:'/',
+            element:<Hero/>
+        },
+        {
+            path:'/soft',
+            element:<Software/>
+        },
+    ]
+}
+])
+// const root=ReactDOM.createRoot(document.querySelector('#root'))
+// root.render(<RouterProvider router={appRouter}/>)
+// export default App
